85095b9c1d54cd0cf3c0fc13898380071c1c55b8
Pedro <pedroph.nunes05@gmail.com>
2026-06-10 11:40:24 -0300

    fix rota front

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sua-api-publica.com',
-  wsUrl: 'wss://sua-api-publica.com',
+  apiUrl: 'https://marketplace-d-production.up.railway.app',
+  wsUrl: 'wss://marketplace-d-production.up.railway.app',
   cloudinaryCloudName: 'seu-cloud-name',
   cloudinaryUploadPreset: 'seu-upload-preset',
 };
